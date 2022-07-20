0b012a5ee62a2c87180f8078f4f6b67c760355c8
AntonParker18 <kozachok2805@gmail.com>
2022-07-20 20:20:08 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./router/routes')
+const path = require('path')
 
 const DB_URL =
   'mongodb+srv://antonparker:123qwe123@cluster0.xc3w1ba.mongodb.net/?retryWrites=true&w=majority'
@@ -10,7 +11,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+
 app.use(cors('*'))
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+
 app.use('/api', routes)
 
 mongoose.connect(
